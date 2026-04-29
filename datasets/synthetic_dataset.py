416256cbc8ea3ad0c55e0c194760984dd9c1e429
bobo <56385159+ANKer661@users.noreply.github.com>
2026-04-29 23:49:29 +0200

    optimize `nlin_map` implementation

--- datasets/synthetic_dataset.py
+++ datasets/synthetic_dataset.py
@@ -42,24 +42,25 @@ def random_nonlin_map(n_in, n_out, n_hidden, rank=100):
     W_0 = make_low_rank_matrix(n_in, n_hidden, effective_rank=rank)
     W_1 = make_low_rank_matrix(n_hidden, n_hidden, effective_rank=rank)
     W_2 = make_low_rank_matrix(n_hidden, n_out, effective_rank=rank)
-    # No biases
-    b_0 = np.random.uniform(0, 0, (1, n_hidden))
-    b_1 = np.random.uniform(0, 0, (1, n_hidden))
-    b_2 = np.random.uniform(0, 0, (1, n_out))
+    # No biases: ?
+    # b_0 = np.random.uniform(0, 0, (1, n_hidden))
+    # b_1 = np.random.uniform(0, 0, (1, n_hidden))
+    # b_2 = np.random.uniform(0, 0, (1, n_out))
+    # nlin_map = lambda x: np.matmul(
+    #     ReLU(
+    #         np.matmul(ReLU(np.matmul(x, W_0) + np.tile(b_0, (x.shape[0], 1))), W_1)
+    #         + np.tile(b_1, (x.shape[0], 1))
+    #     ),
+    #     W_2,
+    # ) + np.tile(b_2, (x.shape[0], 1))
 
-    nlin_map = lambda x: np.matmul(
-        ReLU(
-            np.matmul(ReLU(np.matmul(x, W_0) + np.tile(b_0, (x.shape[0], 1))), W_1)
-            + np.tile(b_1, (x.shape[0], 1))
-        ),
-        W_2,
-    ) + np.tile(b_2, (x.shape[0], 1))
+    def nlin_map(x):
+        h1 = np.maximum(0, x @ W_0)
+        h2 = np.maximum(0, h1 @ W_1)
+
+        return h2 @ W_2
 
     return nlin_map
-
-
-def ReLU(x):
-    return x * (x > 0)
 
 
 def generate_synthetic_data_correlated_c(p: int, n: int, k: int, seed: int):
