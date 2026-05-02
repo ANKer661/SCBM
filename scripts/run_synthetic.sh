1d3ced2dc0ab6d4bcececc86d581890236500348
bobo <56385159+ANKer661@users.noreply.github.com>
2026-05-03 00:28:26 +0200

    workers=0 compile true

--- scripts/run_synthetic.sh
+++ scripts/run_synthetic.sh
@@ -6,7 +6,7 @@ encoder_arch='FCNN'
 
 for i in 42 73 666 777 1009 1279 1597 1811 1949 2053
 do
-  for model in 'AR' 'CEM' 'CBM'
+  for model in 'AR' 'CEM' 'CBM' 
   do
     python -u train.py +model=$model +data=$data \
       experiment_name="${data}_${model}_${i}" seed=$i \
@@ -15,7 +15,7 @@ do
       model.j_epochs=150 model.c_epochs=100 model.t_epochs=50 \
       model.learning_rate=0.0002 model.weight_decay=0.0001 \
       model.train_batch_size=256 model.val_batch_size=512 \
-      workers=8
+      workers=0 model.compile='true'
   done
 
   python -u train.py +model=SCBM +data=$data model.cov_type='amortized' \
@@ -25,7 +25,7 @@ do
     model.j_epochs=150 model.c_epochs=100 model.t_epochs=50\
     model.learning_rate=0.0002 model.weight_decay=0.0001 \
     model.train_batch_size=256 model.val_batch_size=512 \
-    workers=8 model.compile='true'
+    workers=0 model.compile='true'
   python -u train.py +model=SCBM +data=$data model.cov_type='global' \
     model.reg_precision=None experiment_name="${data}_SCBM_global_${i}" \
     seed=$i logging.project=SCBM logging.mode=offline \
@@ -33,5 +33,5 @@ do
     model.j_epochs=150 model.c_epochs=100 model.t_epochs=50 \
     model.learning_rate=0.0002 model.weight_decay=0.0001 \
     model.train_batch_size=256 model.val_batch_size=512 \
-    workers=8 model.compile='true'
+    workers=0 model.compile='true'
 done
