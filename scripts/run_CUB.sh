32e17d0672461eabde7748268f69f672b3767a3c
bobo <56385159+ANKer661@users.noreply.github.com>
2026-05-06 15:38:30 +0200

    update scripts

--- scripts/run_CUB.sh
+++ scripts/run_CUB.sh
@@ -14,8 +14,8 @@ do
       experiment_name="${data}_${model}_${i}" seed=$i \
       logging.project=SCBM logging.mode=offline \
       model.tag=$tag model.encoder_arch=$encoder_arch \
-      model.learning_rate=0.0002 model.weight_decay=0.0001 \
-      model.train_batch_size=256 model.val_batch_size=512 \
+      model.learning_rate=0.0001 model.weight_decay=0.0001 \
+      model.train_batch_size=64 model.val_batch_size=256 \
       workers=8
   done;
   
@@ -25,16 +25,16 @@ do
     model.reg_weight=1 experiment_name="${data}_SCBM_amortized_${i}" \
     seed=$i logging.project=SCBM logging.mode=offline \
     model.tag=$tag model.encoder_arch=$encoder_arch \
-    model.learning_rate=0.0002 model.weight_decay=0.0001 \
-    model.train_batch_size=256 model.val_batch_size=512 \
+    model.learning_rate=0.0001 model.weight_decay=0.0001 \
+    model.train_batch_size=64 model.val_batch_size=256 \
     workers=8 model.compile='true'
   python -u train.py +model=SCBM +data=$data \
     model.cov_type='global' model.reg_precision=None \
     experiment_name="${data}_SCBM_global_${i}" seed=$i \
     logging.project=SCBM logging.mode=offline \
     model.tag=$tag model.encoder_arch=$encoder_arch \
-    model.learning_rate=0.0002 model.weight_decay=0.0001 \
-    model.train_batch_size=256 model.val_batch_size=512 \
+    model.learning_rate=0.0001 model.weight_decay=0.0001 \
+    model.train_batch_size=64 model.val_batch_size=256 \
     workers=8 model.compile='true'
 
 done;
